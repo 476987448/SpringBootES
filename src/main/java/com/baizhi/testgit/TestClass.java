c7e3343df8c3bc433641108f4e63c4a077548fbc
mnf <476987448@qq.com>
2019-10-15 16:24:18 +0800

    马宁飞push2

--- src/main/java/com/baizhi/testgit/TestClass.java
+++ src/main/java/com/baizhi/testgit/TestClass.java
@@ -1,4 +1,9 @@
 package com.baizhi.testgit;
 
 public class TestClass {
+    public static void main(String[] args) {
+        System.out.println("马宁飞1");
+        System.out.println("马宁飞2");
+        System.out.println("马宁飞3");
+    }
 }
