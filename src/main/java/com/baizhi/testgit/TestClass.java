6ab59698f82fafa4ad02931abdd634d502a4735b
lil <981873019@qq.com>
2019-10-15 16:28:04 +0800

    建立了合了一下

--- src/main/java/com/baizhi/testgit/TestClass.java
+++ src/main/java/com/baizhi/testgit/TestClass.java
@@ -3,10 +3,9 @@ package com.baizhi.testgit;
 public class TestClass {
     public static void main(String[] args) {
         System.out.println("1111111111");
-    }
-    public static void main(String[] args) {
         System.out.println("马宁飞1");
         System.out.println("马宁飞2");
         System.out.println("马宁飞3");
     }
+
 }
